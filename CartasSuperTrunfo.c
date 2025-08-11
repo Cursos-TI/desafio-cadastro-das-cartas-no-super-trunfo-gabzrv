9142249d6bafba40b3b59e60c51b7d2ccf07dc01
gabzrv <157644614+gabzrv@users.noreply.github.com>
2025-08-11 11:16:21 -0300

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,57 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-     //Desafio Super Trunfo;
-     // 1- Cadastro de cartas com suas respectivas informações;
-     // 2- Leitura das informações;
-     // 3- Saída das informações;
-
-    typedef struct{
-        char estado[50], cidade[50], codigo[50];
-        float populacao, area, pib;
-        int pontos_turisticos;
-    } Carta;
-     
-    void exibirCarta(Carta carta){
-      printf("Codigo: %s\n", carta.codigo);
-      printf("Estado: %s\n", carta.estado);
-      printf("Cidade: %s\n", carta.cidade);
-      printf("População: %.3f milhões\n", carta.populacao);
-      printf("Área: %.2f km²\n", carta.area);
-      printf("PIB: %.2F bilhões\n", carta.pib);
-      printf("Pontos Turísticos: %d\n\n", carta.pontos_turisticos);
-    }
-     
-    int main(){
-
-        //Cadastro da carta 1
-        Carta carta1 = {
-            .estado = "Bahia",
-            .cidade = "Salvador",
-            .codigo = "A01",
-            .populacao = 2.418, //Em milhões
-            .area = 693.442, //Em Km²
-            .pib = 62.954, //Em bilhões
-            .pontos_turisticos = 15
-        };
-        
-        //Cadastro da carta 2
-        Carta carta2 = {
-            .estado = "Ceara",
-            .cidade = "Fortaleza",
-            .codigo = "B02",
-            .populacao = 2.574, //Em milhões
-            .area = 312.353, //Em Km²
-            .pib = 73.0, //Em bolhões
-            .pontos_turisticos = 25
-        };
-
-        //Exibir as cartas
-        printf("Carta1:\n");
-        exibirCarta(carta1);
-
-        printf("Carta 2: \n");
-        exibirCarta(carta2);
-
-        return 0;
-    }
+int main() {
     
+    //Neste código, iremos criar um jogo de super trunfo em c, contendo o estado, o código da carta, nome da cidade, a população, a área (em km²), o pib e o número de ponstos turísticos//
+
+    //Variáveis
+  char Estado[50],Codigo[10],Nome_da_cidade[100];
+  int Pontos_turisticos;
+  float Area,Pib; 
+  long Populacao;
+
+    //Carta 1
+  strcpy(Estado, "Goiás");
+  strcpy(Nome_da_cidade, "Goiânia");
+  strcpy(Codigo, "A");
+  Populacao = 1437366;
+  Area = 729.296; //Em Km²
+  Pib = 59.865; //Em bilhões
+  Pontos_turisticos = 16;
+
+    //Exibição carta 1
+  printf("Carta 1:\n");
+  printf("Estado: %s\n", Estado);
+  printf("Cidade: %s\n", Nome_da_cidade);
+  printf("Código: %s\n", Codigo);
+  printf("População: %ld\n", Populacao);
+  printf("Área: %.3f km²\n", Area);
+  printf("Pib: %.3f bilhões\n", Pib);
+  printf("Pontos turísticos: %d\n", Pontos_turisticos);
+
+    //Carta 2
+  strcpy(Estado, "Rio Grande do Sul");
+  strcpy(Nome_da_cidade, "Porto Alegre");
+  strcpy(Codigo, "B");
+  Populacao = 1332845;
+  Area = 495.977; //Em km²
+  Pib = 76.074; //Em bilhões
+  Pontos_turisticos = 18;
+
+    // Exibe carta 2
+  printf("Carta 2:\n");
+  printf("Estado: %s\n", Estado);
+  printf("Cidade: %s\n", Nome_da_cidade);
+  printf("Código: %s\n", Codigo);
+  printf("População: %ld\n", Populacao);
+  printf("Área: %.3f km²\n", Area);
+  printf("PIB: %.3f bilhões\n", Pib);
+  printf("Pontos turísticos: %d\n", Pontos_turisticos);
+
+return 0;
+
+}    
